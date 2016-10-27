0eb39dcde56ac3a9830325794804307585b64773
Csaba Szugyiczki <szugyiczkicsaba@gmail.com>
2016-10-27 21:15:04 +0200

    Basic event handling for add button added

--- HUSI/app/src/main/java/hu/paprikapp/husi/MainActivity.java
+++ HUSI/app/src/main/java/hu/paprikapp/husi/MainActivity.java
@@ -1,7 +1,14 @@
 package hu.paprikapp.husi;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText volume = (EditText) findViewById(R.id.volume);
+        final EditText alcohol = (EditText) findViewById(R.id.alcohol);
+        final Button addButton = (Button) findViewById(R.id.add_button);
+        final TextView listText = (TextView) findViewById(R.id.list_textView);
+
+        CheckBox checkbox = (CheckBox) findViewById(R.id.test_check);
+        checkbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Toast.makeText(MainActivity.this, "IsChecked: " + String.valueOf(isChecked), Toast.LENGTH_LONG).show();
+                addButton.setEnabled(isChecked);
+            }
+        });
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String volumeStr = volume.getText().toString();
+                String alcoholStr = alcohol.getText().toString();
+                String volumeAlc = String.format("%s ml - %s%%", volumeStr, alcoholStr);
+                listText.setText(volumeAlc + "\n" + listText.getText().toString());
+            }
+        });
     }
 }
